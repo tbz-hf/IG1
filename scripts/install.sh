96655fe69677eb56def89e71aabe1bad8abe0b2a
Marcel <marcel.bernet@mc-b.ch>
2021-06-16 13:41:02 +0200

    mqtt clients

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,5 +11,8 @@ sudo microk8s kubectl apply -f ~/IG1/DataVolume.yaml
 sudo microk8s kubectl apply -f ~/IG1/mosquitto.yaml
 sudo microk8s kubectl apply -f ~/IG1/nodered.yaml
 
+# MQTT CLI
+sudo apt-get install -y mosquitto-clients
+
 # Introseite 
 bash -x /opt/lernmaas/helper/intro
