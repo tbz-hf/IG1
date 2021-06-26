51f73fade26fb597bf6e072ad690ef245532efb2
Marcel <marcel.bernet@mc-b.ch>
2021-06-26 15:19:07 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,8 @@
 #
 # 
 
-# Daten werden unter /home/ubuntu/data abgelegt. NFS Mount auf Rack Server
-sudo microk8s kubectl apply -f ~/IG1/DataVolume.yaml
-
 # Services
-sudo microk8s kubectl apply -f ~/IG1/mosquitto.yaml
+# sudo microk8s kubectl apply -f ~/IG1/mosquitto.yaml
 sudo microk8s kubectl apply -f ~/IG1/nodered.yaml
 
 # MQTT CLI
